646a4561f940e5a1e426f66ccee0039f0bc03292
Davetto <joshrgarb@gmail.com>
2022-05-08 22:53:29 -0500

    Rick & Morty API using VUE

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Rick & Morty</h1>
+    <FilterByName />
+    <FilterByStatus />
+    <ListCharacters />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListCharacters from "@/components/ListCharacters";
+import FilterByStatus from "@/components/FilterByStatus";
+import FilterByName from "@/components/FilterByName";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ListCharacters,
+    FilterByStatus,
+    FilterByName
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+:root {
+  --background-body: #24282f;
+  --background-card: #3c3e44;
+  --text-white: #ffffff;
+  --text-gray: #828282;
+  --text-orange: #ff9f1c;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: "Helvetica Neue", sans-serif;
+}
+
+h1 {
+  margin: 4rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 3.5rem;
+}
+
+.container {
+  width: 100%;
+  max-width: 90%;
+  margin: 0mm 5rem auto;
+}
+
+img {
+  width: 100%;
 }
 </style>
---
+++ src/components/FilterByName.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search">
+    <input
+      type="text"
+      placeholder="Search By Name"
+      v-model="name"
+      @keyup="filter()"
+    />
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const store = useStore();
+    const name = ref("");
+    const filter = () => {
+      store.dispatch("filterByName", name.value);
+    };
+
+    return { name, filter };
+  },
+};
+</script>
+
+<style lang="scss">
+.search {
+  width: 20%;
+  margin: 3rem auto 0;
+
+  input {
+    width: 100%;
+    height: 3rem;
+    border: none;
+    border-radius: 10px;
+    padding: 0 0.5rem;
+    background-color: #3c3e44;
+    color: #fff;
+
+    &:focus {
+      outline: none;
+      background-color: #fff;
+      color: #3c3e44;
+    }
+  }
+}
+</style>
